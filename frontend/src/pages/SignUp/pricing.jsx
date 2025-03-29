865e564cd61f9a5d5f39adda09a4723748990658
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-03-29 14:44:51 +0700

    feat: integration sign-up

--- frontend/src/pages/SignUp/pricing.jsx
+++ frontend/src/pages/SignUp/pricing.jsx
@@ -1,7 +1,26 @@
-import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar';
+import { Link } from 'react-router-dom';
+import { postSignUp } from '../../services/authService';
+import { useMutation } from '@tanstack/react-query';
 
-export default function Pricing() {
+export default function Pricing({ data }) {
+  const { isLoading, mutateAsync } = useMutation({
+    mutationFn: () => postSignUp(data),
+  });
+
+  const submitData = async () => {
+    try {
+      if (!data) {
+        return;
+      }
+
+      const response = await mutateAsync();
+
+      window.location.replace(response.data.midtrans_payment_url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className='relative flex flex-col flex-1 p-[10px]'>
       <div className='absolute w-[calc(100%-20px)] min-h-[calc(100vh-20px)] h-[calc(100%-20px)] bg-[#060A23] -z-10 rounded-[20px]'>
@@ -154,7 +173,7 @@ export default function Pricing() {
           </div>
           <hr className='border-[#262A56]' />
           <div className='flex flex-col gap-3'>
-            <button>
+            <button onClick={submitData} disabled={isLoading}>
               <div className='flex items-center justify-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]'>
                 <span className='font-semibold text-white'>
                   Choose This Plan
