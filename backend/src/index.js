4dfb3e92cdaa7303e0ceb3f2732c47df28d09830
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-03-29 12:55:41 +0700

    feat: setup conection database

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,15 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import globalRoutes from './routes/globalRoutes.js';
+import connectDB from './utils/database.js';
 
 const app = express();
 
 dotenv.config();
 
-const port = 3000;
+connectDB();
+
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -22,5 +25,5 @@ app.get('/', (req, res) => {
 app.use('/api', globalRoutes);
 
 app.listen(port, () => {
-  console.log('LMS Backend listening on port 3000');
+  console.log(`LMS Backend listening on port ${port}`);
 });
