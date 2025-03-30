cabd56ff2ef1c2ddaf6db17fce60555da2b77316
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-03-30 16:10:23 +0700

    feat: create api sign-in

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 import transactionModel from '../models/transactionModel.js';
 
@@ -61,5 +62,69 @@ export const signUpAction = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ massage: 'Internal server error' });
+  }
+};
+
+export const signInAction = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const existingUser = await userModel
+      .findOne()
+      .where('email')
+      .equals(body.email);
+
+    if (!existingUser) {
+      return res.status(400).json({
+        message: 'User not found',
+      });
+    }
+
+    const comparePassword = bcrypt.compareSync(
+      body.password,
+      existingUser.password
+    );
+
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: 'Email or password incorrect',
+      });
+    }
+
+    const isValidUser = await transactionModel.findOne({
+      user: existingUser._id,
+      status: 'success',
+    });
+
+    if (existingUser.role !== 'student' && !isValidUser) {
+      return res.status(400).json({
+        message: 'User not verified',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        data: {
+          id: existingUser._id.toString(),
+        },
+      },
+      process.env.SECRET_KEY_JWT,
+      { expiresIn: '1 days' }
+    );
+
+    return res.json({
+      message: 'User Logged in success',
+      data: {
+        name: existingUser.name,
+        email: existingUser.email,
+        token,
+        role: existingUser.role,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Internal server error',
+    });
   }
 };
