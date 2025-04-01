b38fdaae2920d01680380a02ce27d5aa3d52e5cc
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-04-01 21:27:39 +0700

    feat: create api create course

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -1,4 +1,8 @@
+import fs from 'fs';
+import categoryModel from '../models/categoryModel.js';
 import courseModel from '../models/courseModel.js';
+import userModel from '../models/userModel.js';
+import { courseSchema } from '../utils/schema.js';
 
 export const getCourses = async (req, res) => {
   try {
@@ -30,6 +34,74 @@ export const getCourses = async (req, res) => {
       message: 'Get Courses Success',
       data: response,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
+};
+
+export const postCourse = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const parse = courseSchema.safeParse(body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map((err) => err.message);
+
+      if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+        fs.unlinkSync(req?.file?.path);
+      }
+
+      return res.status(500).json({
+        message: 'Error Validation',
+        data: null,
+        errors: errorMessages,
+      });
+    }
+
+    const category = await categoryModel.findById(parse.data.categoryId);
+
+    if (!category) {
+      return res.status(500).json({
+        message: 'Category Id not found',
+      });
+    }
+
+    const course = new courseModel({
+      name: parse.data.name,
+      category: category._id,
+      description: parse.data.description,
+      tagline: parse.data.tagline,
+      thumbnail: req.file?.filename,
+      manager: req.user._id,
+    });
+
+    await course.save();
+
+    await categoryModel.findByIdAndUpdate(
+      category._id,
+      {
+        $push: {
+          courses: course._id,
+        },
+      },
+      { new: true }
+    );
+
+    await userModel.findByIdAndUpdate(
+      req.user?._id,
+      {
+        $push: {
+          courses: course._id,
+        },
+      },
+      { new: true }
+    );
+
+    return res.json({ message: 'Create Course Success' });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import multer from 'multer';
 import { verifyToken } from '../middlewares/verifyToken.js';
-import { getCourses } from '../controllers/courseController.js';
+import { getCourses, postCourse } from '../controllers/courseController.js';
+import { fileFilter, fileStorageCourse } from '../utils/multer.js';
 
 const courseRoutes = express.Router();
 
+const uploads = multer({
+  storage: fileStorageCourse,
+  fileFilter: fileFilter,
+});
+
 courseRoutes.get('/courses', verifyToken, getCourses);
+courseRoutes.post(
+  '/courses',
+  verifyToken,
+  uploads.single('thumbnail'),
+  postCourse
+);
 
 export default courseRoutes;
