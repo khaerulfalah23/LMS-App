0d2505a3314640a61c4927a4b5e412cea6e4ee3c
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-04-03 07:33:20 +0700

    feat: slicing create course page

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, redirect } from 'react-router-dom';
+import { MANAGER_SESSION, STORAGE_KEY } from '../utils/const';
 import SignUpPage from '../pages/SignUp';
 import SignInPage from '../pages/SignIn';
 import SuccessCheckoutPage from '../pages/SuccessCheckout';
 import LayoutDashboard from '../components/layout';
-import { MANAGER_SESSION, STORAGE_KEY } from '../utils/const';
 import secureLocalStorage from 'react-secure-storage';
+import ManageCoursePage from '../pages/manager/courses';
+import ManageCreateCoursePage from '../pages/manager/create-courses';
 
 const router = createBrowserRouter([
   {
@@ -48,6 +50,14 @@ const router = createBrowserRouter([
             <h1>hello</h1>
           </div>
         ),
+      },
+      {
+        path: '/manager/courses',
+        element: <ManageCoursePage />,
+      },
+      {
+        path: '/manager/courses/create',
+        element: <ManageCreateCoursePage />,
       },
     ],
   },
